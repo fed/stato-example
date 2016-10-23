9ff216d65795011967b073fa5d0b65c13ce821b8
Federico Knüssel <fknussel@gmail.com>
2016-10-23 12:07:07 +1100

    Move actions and reducers out of the Baconify method

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
 import {baconify} from '../lib/baconify';
+import store from '../lib/store';
+import getReducer from './reducers';
 
 const initialState = {
   items: [
@@ -26,4 +28,9 @@ const initialState = {
 
 baconify(initialState, (props) => {
   ReactDOM.render(<App {...props} />, document.getElementById('app'));
-});
+},
+  [store.stream('CREATE_ITEM')], getReducer('CREATE_ITEM'),
+  [store.stream('REMOVE_ITEM')], getReducer('REMOVE_ITEM'),
+  [store.stream('MARK_ITEM_AS_DONE')], getReducer('MARK_ITEM_AS_DONE'),
+  [store.stream('ADD_COUNTRY')], getReducer('ADD_COUNTRY')
+);
